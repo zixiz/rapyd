9fab58eedc0d27e98ee8f073f5c03fc4d8a0e2e9
zixiz <gshachargilad@gmail.com>
2020-02-11 20:44:10 +0200

    Changed Read Me

--- routes/funds.js
+++ routes/funds.js
@@ -4,10 +4,14 @@ var router = express.Router();
 const addFunds = require('../controllers/addFunds');
 const transferFunds = require('../controllers/transferFunds');
 const transferResponse = require('../controllers/transferResponse');
+
+//Add funds up to 5 eWallets, read more in README.md file "Add Funds"
 router.post('/add',addFunds);
 
+//Transfer funds between eWallets, read more in README.md file "Transfer Funds Between eWallets"
 router.post('/transfer', transferFunds);
 
+// Response transfer, read more in README.md file "Cancel/Accept/Decline Transfer API"
 router.post('/transfer/response', transferResponse);
 
 module.exports = router;
--- routes/wallet.js
+++ routes/wallet.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const createEwallet = require('../controllers/createEwallet');
 
+//This Router Create eWallet, read more in README.md file "Create Ewallet"
 
 router.post('/ewallet',createEwallet);
 
